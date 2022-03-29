41b4588206b617c141210f99089efce96480fa4b
sourbeatles3 <sourbeatles3@gmail.com>
2022-03-29 14:53:21 +0900

    열두번째 작업

--- python/quest.py
+++ python/quest.py
@@ -1,8 +1,9 @@
-suc3 = int(input('몇 개의 3점슛을 성공시켰니?  '))
-try3 = int(input('몇 번의 3점슛을 시도했니?  '))
-def threeP(a, b):
-    if(a > b):
-        print('네가 시도한 3점슛보다 성공한 3점슛이 많을리 없어')
+시도 = int(input('몇 개의 슛을 시도했니?  '))
+성공 = int(input('몇 개의 슛을 성공했니?  '))
+성공률 = (성공/시도) * 100
+def shot(a, b):
+    if(시도 >= 성공):
+        print(f'네 슛 성공률은 {성공률}%구나')
     else:
-        return a/b
-print(threeP(suc3, try3))
+        print('네가 시도한 슛보다 많이 성공했을리가 없어')
+print(shot(시도, 성공))
